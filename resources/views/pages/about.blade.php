04123d0b50f6128e998c4d529b03d769e2e87c48
Łukasz Mól <kiwikdd@gmail.com>
2019-10-28 19:38:51 +0100

    inital commit of the project

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class='card'>
+        <div col-sm-6>
+            About this page
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>This is the forum about PCs</h1>
+</div>
+@endsection
